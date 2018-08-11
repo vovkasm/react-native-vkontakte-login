d115f58494bbb489174ae5510c550dd72ef82dbc
Vladimir Timofeev <vovkasm@gmail.com>
2018-08-12 00:44:59 +0300

    AppDelegate for RN

--- samples/VkLoginSample/ios/VkLoginSample/VkLoginSample/AppDelegate.m
+++ samples/VkLoginSample/ios/VkLoginSample/VkLoginSample/AppDelegate.m
@@ -1,13 +1,24 @@
 #import "AppDelegate.h"
 
-@interface AppDelegate ()
-
-@end
+#import <React/RCTBundleURLProvider.h>
+#import <React/RCTRootView.h>
 
 @implementation AppDelegate
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
-    // Override point for customization after application launch.
+    NSURL* jsCodeLocation = [[RCTBundleURLProvider sharedSettings] jsBundleURLForBundleRoot:@"index" fallbackResource:nil];
+
+    RCTRootView *rootView = [[RCTRootView alloc] initWithBundleURL:jsCodeLocation
+                                                        moduleName:@"VkLoginSample"
+                                                 initialProperties:nil
+                                                     launchOptions:launchOptions];
+    rootView.backgroundColor = [[UIColor alloc] initWithRed:1.0f green:1.0f blue:1.0f alpha:1];
+
+    self.window = [[UIWindow alloc] initWithFrame:[UIScreen mainScreen].bounds];
+    UIViewController *rootViewController = [UIViewController new];
+    rootViewController.view = rootView;
+    self.window.rootViewController = rootViewController;
+    [self.window makeKeyAndVisible];
     return YES;
 }
 
