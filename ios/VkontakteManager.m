d829c3c1657fc756ecc39d2f73a7be125d750bde
Vladimir Timofeev <vovkasm@gmail.com>
2018-08-14 19:49:14 +0300

    npm run prettier

--- ios/VkontakteManager.m
+++ ios/VkontakteManager.m
@@ -20,182 +20,180 @@
 #define DMLog(...) do { } while (0)
 #endif
 
-NSString *const E_NOT_INITIALIZED = @"E_NOT_INITIALIZED";
-NSString *const E_VK_UNKNOWN = @"E_VK_UNKNOWN";
-NSString *const E_VK_API_ERROR = @"E_VK_API_ERROR";
-NSString *const E_VK_CANCELED = @"E_VK_CANCELED";
-NSString *const E_VK_RESPONSE_STRING_PARSING_ERROR = @"E_VK_RESPONSE_STRING_PARSING_ERROR";
-NSString *const E_VK_AUTHORIZE_CONTROLLER_CANCEL = @"E_VK_AUTHORIZE_CONTROLLER_CANCEL";
-NSString *const E_VK_REQUEST_NOT_PREPARED = @"E_VK_REQUEST_NOT_PREPARED";
+NSString* const E_NOT_INITIALIZED = @"E_NOT_INITIALIZED";
+NSString* const E_VK_UNKNOWN = @"E_VK_UNKNOWN";
+NSString* const E_VK_API_ERROR = @"E_VK_API_ERROR";
+NSString* const E_VK_CANCELED = @"E_VK_CANCELED";
+NSString* const E_VK_RESPONSE_STRING_PARSING_ERROR = @"E_VK_RESPONSE_STRING_PARSING_ERROR";
+NSString* const E_VK_AUTHORIZE_CONTROLLER_CANCEL = @"E_VK_AUTHORIZE_CONTROLLER_CANCEL";
+NSString* const E_VK_REQUEST_NOT_PREPARED = @"E_VK_REQUEST_NOT_PREPARED";
 
 @implementation VkontakteManager {
-  VKSdk *sdk;
-  RCTPromiseResolveBlock loginResolver;
-  RCTPromiseRejectBlock loginRejector;
+    VKSdk* sdk;
+    RCTPromiseResolveBlock loginResolver;
+    RCTPromiseRejectBlock loginRejector;
 }
 
-- (instancetype)init
-{
-  if (self = [super init]) {
-    NSNumber *appId = [[NSBundle mainBundle] objectForInfoDictionaryKey:@"VK_APP_ID"];
-    if (appId){
-      DMLog(@"Found appId %@ on startup", appId);
-      [self initialize:appId];
+- (instancetype)init {
+    if (self = [super init]) {
+        NSNumber* appId = [[NSBundle mainBundle] objectForInfoDictionaryKey:@"VK_APP_ID"];
+        if (appId) {
+            DMLog(@"Found appId %@ on startup", appId);
+            [self initialize:appId];
+        }
     }
-  }
-  return self;
+    return self;
 }
 
 RCT_EXPORT_MODULE();
 
 - (dispatch_queue_t)methodQueue {
-  return dispatch_get_main_queue();
+    return dispatch_get_main_queue();
 }
 
 RCT_EXPORT_METHOD(initialize:(nonnull NSNumber*)appId) {
-  DMLog(@"Initialize app id %@", appId);
+    DMLog(@"Initialize app id %@", appId);
 
-  sdk = [VKSdk initializeWithAppId:[appId stringValue]];
-  [sdk registerDelegate:self];
-  [sdk setUiDelegate:self];
+    sdk = [VKSdk initializeWithAppId:[appId stringValue]];
+    [sdk registerDelegate:self];
+    [sdk setUiDelegate:self];
 }
 
 RCT_EXPORT_METHOD(login:(NSArray<NSString*>*)scope resolver:(RCTPromiseResolveBlock)resolve rejecter:(RCTPromiseRejectBlock)reject) {
-  DMLog(@"Login with scope %@", scope);
-  if (![VKSdk initialized]){
-    reject(E_NOT_INITIALIZED, @"VK SDK must be initialized first", nil);
-    return;
-  }
-
-  self->loginResolver = resolve;
-  self->loginRejector = reject;
-  [VKSdk wakeUpSession:scope completeBlock:^(VKAuthorizationState state, NSError *error) {
-    switch (state) {
-      case VKAuthorizationAuthorized: {
-        DMLog(@"User already authorized");
-        NSDictionary *loginData = [self getResponse];
-        self->loginResolver(loginData);
-        break;
-      }
-      case VKAuthorizationInitialized: {
-        DMLog(@"Authorization required");
-        [VKSdk authorize:scope];
-        break;
-      }
-      case VKAuthorizationError: {
-        DMLog(@"VK Authorization error: %@", [error localizedDescription]);
-        [self rejectLoginWithError:error];
-      }
+    DMLog(@"Login with scope %@", scope);
+    if (![VKSdk initialized]) {
+        reject(E_NOT_INITIALIZED, @"VK SDK must be initialized first", nil);
+        return;
     }
-  }];
+
+    self->loginResolver = resolve;
+    self->loginRejector = reject;
+    [VKSdk wakeUpSession:scope completeBlock:^(VKAuthorizationState state, NSError* error) {
+         switch (state) {
+             case VKAuthorizationAuthorized: {
+                 DMLog(@"User already authorized");
+                 NSDictionary* loginData = [self getResponse];
+                 self->loginResolver(loginData);
+                 break;
+             }
+             case VKAuthorizationInitialized: {
+                 DMLog(@"Authorization required");
+                 [VKSdk authorize:scope];
+                 break;
+             }
+             case VKAuthorizationError: {
+                 DMLog(@"VK Authorization error: %@", [error localizedDescription]);
+                 [self rejectLoginWithError:error];
+             }
+         }
+     }];
 };
 
 RCT_EXPORT_METHOD(isLoggedIn:(RCTPromiseResolveBlock)resolve rejecter:(RCTPromiseRejectBlock)reject) {
-  if ([VKSdk initialized]){
-    resolve([NSNumber numberWithBool:[VKSdk isLoggedIn]]);
-  }
-  else {
-    reject(E_NOT_INITIALIZED, @"VK SDK must be initialized first", nil);
-  }
+    if ([VKSdk initialized]) {
+        resolve([NSNumber numberWithBool:[VKSdk isLoggedIn]]);
+    } else {
+        reject(E_NOT_INITIALIZED, @"VK SDK must be initialized first", nil);
+    }
 }
 
 RCT_REMAP_METHOD(logout, resolver:(RCTPromiseResolveBlock)resolve rejecter:(RCTPromiseRejectBlock)reject){
-  DMLog(@"Logout");
-  if (![VKSdk initialized]){
-    reject(E_NOT_INITIALIZED, @"VK SDK must be initialized first", nil);
-    return;
-  }
-  [VKSdk forceLogout];
-  resolve(nil);
+    DMLog(@"Logout");
+    if (![VKSdk initialized]) {
+        reject(E_NOT_INITIALIZED, @"VK SDK must be initialized first", nil);
+        return;
+    }
+    [VKSdk forceLogout];
+    resolve(nil);
 };
 
-- (void)vkSdkAccessAuthorizationFinishedWithResult:(VKAuthorizationResult *)result {
-  DMLog(@"Authorization result is %@", result);
-  if (result.error && self->loginRejector != nil) {
-    DMLog(@"Authrization failed with code %ld and message %ld", (long)result.error.code, (long)result.error.vkError.errorCode);
-    [self rejectLoginWithError:result.error];
-  } else if (result.token && self->loginResolver != nil) {
-    NSDictionary *loginData = [self getResponse];
-    self->loginResolver(loginData);
-  }
+- (void)vkSdkAccessAuthorizationFinishedWithResult:(VKAuthorizationResult*)result {
+    DMLog(@"Authorization result is %@", result);
+    if (result.error && self->loginRejector != nil) {
+        DMLog(@"Authrization failed with code %ld and message %ld", (long)result.error.code, (long)result.error.vkError.errorCode);
+        [self rejectLoginWithError:result.error];
+    } else if (result.token && self->loginResolver != nil) {
+        NSDictionary* loginData = [self getResponse];
+        self->loginResolver(loginData);
+    }
 }
 
 - (void)vkSdkUserAuthorizationFailed {
-  DMLog(@"Authrization failed");
-  self->loginRejector(E_VK_UNKNOWN, @"User Authorization Failed delegate method called", nil);
+    DMLog(@"Authrization failed");
+    self->loginRejector(E_VK_UNKNOWN, @"User Authorization Failed delegate method called", nil);
 }
 
-- (void)vkSdkNeedCaptchaEnter:(VKError *)captchaError {
-  DMLog(@"VK SDK UI Delegate needs captcha: %@", captchaError);
-  VKCaptchaViewController *vc = [VKCaptchaViewController captchaControllerWithError:captchaError];
+- (void)vkSdkNeedCaptchaEnter:(VKError*)captchaError {
+    DMLog(@"VK SDK UI Delegate needs captcha: %@", captchaError);
+    VKCaptchaViewController* vc = [VKCaptchaViewController captchaControllerWithError:captchaError];
 
-  UIViewController *root = [[[[UIApplication sharedApplication] delegate] window] rootViewController];
+    UIViewController* root = [[[[UIApplication sharedApplication] delegate] window] rootViewController];
 
-  [vc presentIn:root];
+    [vc presentIn:root];
 }
 
-- (void)vkSdkShouldPresentViewController:(UIViewController *)controller {
-  DMLog(@"Presenting view controller");
-  UIViewController *root = [[[[UIApplication sharedApplication] delegate] window] rootViewController];
+- (void)vkSdkShouldPresentViewController:(UIViewController*)controller {
+    DMLog(@"Presenting view controller");
+    UIViewController* root = [[[[UIApplication sharedApplication] delegate] window] rootViewController];
 
-  [root presentViewController:controller animated:YES completion:nil];
+    [root presentViewController:controller animated:YES completion:nil];
 }
 
-- (NSDictionary *)getResponse {
-  VKAccessToken *token = [VKSdk accessToken];
+- (NSDictionary*)getResponse {
+    VKAccessToken* token = [VKSdk accessToken];
 
-  if (token) {
-    return @{
-        @"access_token" : token.accessToken,
-        @"user_id" : token.userId,
-        @"expires_in" : [NSNumber numberWithInteger:token.expiresIn],
-        @"email" : token.email ?: [NSNull null],
-        @"secret" : token.secret ?: [NSNull null]
-    };
-  }
-  else {
-    return [NSNull null];
-  }
+    if (token) {
+        return @{
+                   @"access_token" : token.accessToken,
+                   @"user_id" : token.userId,
+                   @"expires_in" : [NSNumber numberWithInteger:token.expiresIn],
+                   @"email" : token.email ? : [NSNull null],
+                   @"secret" : token.secret ? : [NSNull null]
+        };
+    } else {
+        return [NSNull null];
+    }
 }
 
 + (BOOL)requiresMainQueueSetup {
-  return YES;
+    return YES;
 }
 
--(void)rejectLoginWithError:(NSError*)error {
-  if ([error.domain isEqualToString:VKSdkErrorDomain]) {
-    [self rejectLoginWithVKError:error.vkError];
-  } else {
-    self->loginRejector(E_VK_UNKNOWN, error.localizedDescription, nil);
-  }
+- (void)rejectLoginWithError:(NSError*)error {
+    if ([error.domain isEqualToString:VKSdkErrorDomain]) {
+        [self rejectLoginWithVKError:error.vkError];
+    } else {
+        self->loginRejector(E_VK_UNKNOWN, error.localizedDescription, nil);
+    }
 }
 
--(void)rejectLoginWithVKError:(VKError*)vkError {
-  NSString* errorCode = E_VK_UNKNOWN;
-  NSString* errorMessage = vkError.errorMessage;
-  switch (vkError.errorCode) {
-    case VK_API_ERROR:
-      errorCode = E_VK_API_ERROR;
-      break;
-    case VK_API_CANCELED:
-      errorCode = E_VK_CANCELED;
-      errorMessage = @"User canceled";
-      break;
-    case VK_API_REQUEST_NOT_PREPARED:
-      errorCode = E_VK_REQUEST_NOT_PREPARED;
-      break;
-    case VK_RESPONSE_STRING_PARSING_ERROR:
-      errorCode = E_VK_RESPONSE_STRING_PARSING_ERROR;
-      break;
-    case VK_AUTHORIZE_CONTROLLER_CANCEL:
-      errorCode = E_VK_AUTHORIZE_CONTROLLER_CANCEL;
-      break;
-        
-    default:
-      errorCode = E_VK_UNKNOWN;
-      break;
-  }
-  self->loginRejector(errorCode, errorMessage, nil);
-}
+- (void)rejectLoginWithVKError:(VKError*)vkError {
+    NSString* errorCode = E_VK_UNKNOWN;
+    NSString* errorMessage = vkError.errorMessage;
+
+    switch (vkError.errorCode) {
+        case VK_API_ERROR:
+            errorCode = E_VK_API_ERROR;
+            break;
+        case VK_API_CANCELED:
+            errorCode = E_VK_CANCELED;
+            errorMessage = @"User canceled";
+            break;
+        case VK_API_REQUEST_NOT_PREPARED:
+            errorCode = E_VK_REQUEST_NOT_PREPARED;
+            break;
+        case VK_RESPONSE_STRING_PARSING_ERROR:
+            errorCode = E_VK_RESPONSE_STRING_PARSING_ERROR;
+            break;
+        case VK_AUTHORIZE_CONTROLLER_CANCEL:
+            errorCode = E_VK_AUTHORIZE_CONTROLLER_CANCEL;
+            break;
+
+        default:
+            errorCode = E_VK_UNKNOWN;
+            break;
+    } /* switch */
+    self->loginRejector(errorCode, errorMessage, nil);
+} /* rejectLoginWithVKError */
 
 @end
--- ios/VkontakteSharing.m
+++ ios/VkontakteSharing.m
@@ -36,70 +36,71 @@
 
 @synthesize bridge = _bridge;
 
-- (void)openShareDlg:(VKShareDialogController *)dialog resolver:(RCTPromiseResolveBlock)resolve rejecter:(RCTPromiseRejectBlock)reject {
-  UIViewController *root = [[[[UIApplication sharedApplication] delegate] window] rootViewController];
-  [dialog setCompletionHandler:^(VKShareDialogController *dialog, VKShareDialogControllerResult result) {
-    if (result == VKShareDialogControllerResultDone) {
-      DMLog(@"onVkShareComplete");
-      resolve(dialog.postId);
-      // done
-    } else if (result == VKShareDialogControllerResultCancelled) {
-      DMLog(@"onVkShareCancel");
-      reject(RCTErrorUnspecified, nil, RCTErrorWithMessage(@"canceled"));
-    }
-  }];
+- (void)openShareDlg:(VKShareDialogController*)dialog resolver:(RCTPromiseResolveBlock)resolve rejecter:(RCTPromiseRejectBlock)reject {
+    UIViewController* root = [[[[UIApplication sharedApplication] delegate] window] rootViewController];
 
-  [root presentViewController:dialog animated:YES completion:nil];
+    [dialog setCompletionHandler:^(VKShareDialogController* dialog, VKShareDialogControllerResult result) {
+         if (result == VKShareDialogControllerResultDone) {
+             DMLog(@"onVkShareComplete");
+             resolve(dialog.postId);
+             // done
+         } else if (result == VKShareDialogControllerResultCancelled) {
+             DMLog(@"onVkShareCancel");
+             reject(RCTErrorUnspecified, nil, RCTErrorWithMessage(@"canceled"));
+         }
+     }];
+
+    [root presentViewController:dialog animated:YES completion:nil];
 }
 
 RCT_EXPORT_MODULE();
 
 - (dispatch_queue_t)methodQueue {
-  return dispatch_get_main_queue();
+    return dispatch_get_main_queue();
 }
 
-RCT_EXPORT_METHOD(share:(NSDictionary *)data resolver:(RCTPromiseResolveBlock)resolve rejecter:(RCTPromiseRejectBlock)reject) {
-  DMLog(@"Open Share Dialog");
-  if (![VKSdk initialized]){
-    reject(RCTErrorUnspecified, nil, RCTErrorWithMessage(@"VK SDK must be initialized first"));
-    return;
-  }
+RCT_EXPORT_METHOD(share:(NSDictionary*)data resolver:(RCTPromiseResolveBlock)resolve rejecter:(RCTPromiseRejectBlock)reject) {
+    DMLog(@"Open Share Dialog");
+    if (![VKSdk initialized]) {
+        reject(RCTErrorUnspecified, nil, RCTErrorWithMessage(@"VK SDK must be initialized first"));
+        return;
+    }
 
-  NSString *imagePath = data[@"image"];
-  NSArray<NSString*>* permissions = @[VK_PER_WALL];
-  if (imagePath != nil && imagePath.length){
-    permissions = [permissions arrayByAddingObject:VK_PER_PHOTOS];
-  }
-  VKSdk *sdk = [VKSdk instance];
-  if (![sdk hasPermissions:permissions]){
-    reject(RCTErrorUnspecified, nil, RCTErrorWithMessage(@"Access denied: no access to call this method"));
-    return;
-  }
+    NSString* imagePath = data[@"image"];
+    NSArray<NSString*>* permissions = @[VK_PER_WALL];
+    if (imagePath != nil && imagePath.length) {
+        permissions = [permissions arrayByAddingObject:VK_PER_PHOTOS];
+    }
+    VKSdk* sdk = [VKSdk instance];
+    if (![sdk hasPermissions:permissions]) {
+        reject(RCTErrorUnspecified, nil, RCTErrorWithMessage(@"Access denied: no access to call this method"));
+        return;
+    }
 
-  VKShareDialogController * shareDialog = [VKShareDialogController new];
-  shareDialog.text = [RCTConvert NSString:data[@"description"]];
-  shareDialog.shareLink = [[VKShareLink alloc] initWithTitle:[RCTConvert NSString:data[@"linkText"]]
-                                               link:[NSURL URLWithString:[RCTConvert NSString:data[@"linkUrl"]]]];
-  shareDialog.dismissAutomatically = YES;
+    VKShareDialogController* shareDialog = [VKShareDialogController new];
+    shareDialog.text = [RCTConvert NSString:data[@"description"]];
+    shareDialog.shareLink = [[VKShareLink alloc] initWithTitle:[RCTConvert NSString:data[@"linkText"]]
+                                                          link:[NSURL URLWithString:[RCTConvert NSString:data[@"linkUrl"]]]];
+    shareDialog.dismissAutomatically = YES;
 
-  if (imagePath.length && _bridge.imageLoader) {
-    RCTImageSource *source = [RCTConvert RCTImageSource:data[@"image"]];
+    if (imagePath.length && _bridge.imageLoader) {
+        RCTImageSource* source = [RCTConvert RCTImageSource:data[@"image"]];
 
-    [_bridge.imageLoader loadImageWithURLRequest:source.request callback:^(NSError *error, UIImage *image) {
-      if (image == nil) {
-        NSLog(@"Failed to load image");
-      } else {
-        VKUploadImage *VKImage = [[VKUploadImage alloc] init];
-        VKImage.sourceImage = image;
-        shareDialog.uploadImages = @[VKImage];
-      }
-      dispatch_async(self.methodQueue, ^{
+        [_bridge.imageLoader loadImageWithURLRequest:source.request callback:^(NSError* error, UIImage* image) {
+             if (image == nil) {
+                 NSLog(@"Failed to load image");
+             } else {
+                 VKUploadImage* VKImage = [[VKUploadImage alloc] init];
+                 VKImage.sourceImage = image;
+                 shareDialog.uploadImages = @[VKImage];
+             }
+             dispatch_async(self.methodQueue, ^{
+                            [self openShareDlg:shareDialog resolver:resolve rejecter:reject];
+                        });
+         }];
+    } else {
         [self openShareDlg:shareDialog resolver:resolve rejecter:reject];
-      });
-    }];
-  } else {
-    [self openShareDlg:shareDialog resolver:resolve rejecter:reject];
-  }
+    }
 }
 
 @end
