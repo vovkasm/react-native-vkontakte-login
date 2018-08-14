b4900bc56e6846b531910a2e7d36ff50884420c7
Vladimir Timofeev <vovkasm@gmail.com>
2018-08-14 12:23:00 +0300

    sample app(ios): AppDelegate part of sdk integration

--- samples/VkLoginSample/ios/VkLoginSample/VkLoginSample/AppDelegate.m
+++ samples/VkLoginSample/ios/VkLoginSample/VkLoginSample/AppDelegate.m
@@ -3,6 +3,8 @@
 #import <React/RCTBundleURLProvider.h>
 #import <React/RCTRootView.h>
 
+#import "VKSdk.h"
+
 @implementation AppDelegate
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
@@ -20,6 +22,11 @@
     self.window.rootViewController = rootViewController;
     [self.window makeKeyAndVisible];
     return YES;
+}
+
+- (BOOL)application:(UIApplication *)app openURL:(NSURL *)url options:(NSDictionary<UIApplicationOpenURLOptionsKey,id> *)options {
+    BOOL ret = [VKSdk processOpenURL:url fromApplication:options[UIApplicationOpenURLOptionsSourceApplicationKey]];
+    return ret;
 }
 
 @end
